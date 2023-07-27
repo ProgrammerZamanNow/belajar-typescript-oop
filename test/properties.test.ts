f7199ab8eb711f45a09e254d29576201f7ea4e57
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-27 23:19:31 +0700

    Method

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -9,6 +9,10 @@ describe('Properties', () => {
             this.id = id;
             this.name = name;
         }
+
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}`);
+        }
     }
 
     it('should can have properties', () => {
@@ -21,5 +25,10 @@ describe('Properties', () => {
         console.info(customer.age);
         console.info(customer);
 
+    });
+
+    it('should can have methods', () => {
+        const customer = new Customer(1, "Eko");
+        customer.sayHello("Budi");
     });
 });
