3e3ce51b11534bd684fe9b2acfd34821d9c47bc4
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-27 23:07:29 +0700

    Constructor

--- test/class.test.ts
+++ test/class.test.ts
@@ -1,16 +1,22 @@
 describe('Class', () => {
+
+    class Customer {
+        constructor() {
+            console.info("Create new customer");
+        }
+    }
+
+    class Order {
+
+    }
+
     it('should can create class', () => {
-
-        class Customer {
-
-        }
-
-        class Order {
-
-        }
-
         const customer: Customer = new Customer();
         const order = new Order();
+    });
 
+    it('should can create constructors', () => {
+        new Customer();
+        new Customer();
     });
 });
