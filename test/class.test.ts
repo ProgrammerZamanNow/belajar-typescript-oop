3b9d74e84f6c28afe235c672f0c81bb57c4324bc
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-27 23:01:09 +0700

    Class

---
+++ test/class.test.ts
@@ -0,0 +1,16 @@
+describe('Class', () => {
+    it('should can create class', () => {
+
+        class Customer {
+
+        }
+
+        class Order {
+
+        }
+
+        const customer: Customer = new Customer();
+        const order = new Order();
+
+    });
+});
