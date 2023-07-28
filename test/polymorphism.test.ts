0929b72b5334b01b4537cd1ac467d70e48956d9c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-28 21:30:09 +0700

    Type CAst

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -14,7 +14,27 @@ describe('Polymorphism', () => {
     }
 
     function sayHello(employee: Employee): void {
-        console.info(`Hello ${employee.name}`);
+        if (employee instanceof VicePresident) {
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`);
+        } else if (employee instanceof Manager) {
+            const manager = employee as Manager;
+            console.info(`Hello manager ${manager.name}`);
+        } else {
+            console.info(`Hello employee ${employee.name}`);
+        }
+    }
+
+    function sayHelloWrong(employee: Employee): void {
+        if (employee instanceof Manager) {
+            const manager = employee as Manager;
+            console.info(`Hello manager ${manager.name}`);
+        } else if (employee instanceof VicePresident) {
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`);
+        } else {
+            console.info(`Hello employee ${employee.name}`);
+        }
     }
 
     it('should support', () => {
@@ -32,5 +52,11 @@ describe('Polymorphism', () => {
         sayHello(new Employee("Eko"));
         sayHello(new Manager("Budi"));
         sayHello(new VicePresident("Joko"));
+    });
+
+    it('should support method parameter polymorphism wrong', () => {
+        sayHelloWrong(new Employee("Eko"));
+        sayHelloWrong(new Manager("Budi"));
+        sayHelloWrong(new VicePresident("Joko"));
     });
 });
