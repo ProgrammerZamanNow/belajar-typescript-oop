92d1158be60b5a44c54a2b6e33a7899a4911c8b2
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-28 17:46:08 +0700

    Method Overriding

---
+++ test/method-overriding.test.ts
@@ -0,0 +1,30 @@
+describe('Method Overriding', () => {
+
+    class Employee {
+        name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+
+        sayHello(name: string): void{
+            console.info(`Hello ${name}, my name is ${this.name}`);
+        }
+    }
+
+    class Manager extends Employee {
+
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}, I am your manager`);
+        }
+    }
+
+
+    it('should support', () => {
+        const employee = new Employee("Eko");
+        employee.sayHello("Budi");
+
+        const manager = new Manager("Eko");
+        manager.sayHello("Budi");
+    });
+});
